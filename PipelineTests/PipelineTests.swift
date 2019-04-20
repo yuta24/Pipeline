9230421a3a3cb5e0944ef8eba59da714414399e5
yuta24 <yuta24@bivre.com>
2019-04-20 15:16:49 +0900

    Fixed

--- PipelineTests/PipelineTests.swift
+++ PipelineTests/PipelineTests.swift
@@ -2,7 +2,7 @@
 //  PipelineTests.swift
 //  PipelineTests
 //
-//  Created by Yu Tawata on 2019/04/13.
+//  Created by Yu Tawata on 2019/04/20.
 //  Copyright © 2019 Yu Tawata. All rights reserved.
 //
 
@@ -19,74 +19,16 @@ class PipelineTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    struct Input: Equatable {
-        enum ValidationError: Swift.Error {
-            case nameEmpty
-            case nameLengthTooLong
-            case emailEmpty
-        }
-
-        var name: String
-        var email: String
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
 
-    func validateInput(_ input: Input) -> Result<Input, Input.ValidationError> {
-        if input.name.isEmpty {
-            return .failure(.nameEmpty)
-        } else if input.email.isEmpty {
-            return .failure(.emailEmpty)
-        } else {
-            return .success(input)
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
         }
     }
 
-    func validate1(_ input: Input) -> Result<Input, Input.ValidationError> {
-        if input.name.isEmpty {
-            return .failure(.nameEmpty)
-        } else {
-            return .success(input)
-        }
-    }
-
-    func validate2(_ input: Input) -> Result<Input, Input.ValidationError> {
-        if input.name.count > 50 {
-            return .failure(.nameLengthTooLong)
-        } else {
-            return .success(input)
-        }
-    }
-
-    func validate3(_ input: Input) -> Result<Input, Input.ValidationError> {
-        if input.email.isEmpty {
-            return .failure(.emailEmpty)
-        } else {
-            return .success(input)
-        }
-    }
-
-    func testPipe1() {
-        let _validate1 = curry(validate1)
-        let _validate2 = curry(bind)(validate2)
-        let _validate3 = curry(bind)(validate3)
-        let inputValidator = _validate1 >>> _validate2 >>> _validate3
-
-        let result = inputValidator(Input(name: "", email: ""))
-        let expectResult = Result<Input, Input.ValidationError>.failure(.nameEmpty)
-
-        XCTAssertNil(result.value)
-        XCTAssertEqual(result.error, expectResult.error)
-    }
-
-    func testPipe2() {
-        let _validate1 = curry(validate1)
-        let _validate2 = curry(bind)(validate2)
-        let _validate3 = curry(bind)(validate3)
-        let inputValidator = _validate1 >>> _validate2 >>> _validate3
-
-        let result = inputValidator(Input(name: "yuta24", email: "yuta24@bivre.com"))
-        let expectResult = Result<Input, Input.ValidationError>.success(Input(name: "yuta24", email: "yuta24@bivre.com"))
-
-        XCTAssertEqual(result.value, expectResult.value)
-        XCTAssertNil(result.error)
-    }
 }
